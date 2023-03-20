28867b090a7a469206a76d3aedd76a3f7f9b0279
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-03-19 22:22:04 -0400

    inicializacion de repositorio

---
+++ variables.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let numero = 2666;
+    let frase = "Rust Programming Language";
+    let f: f64 = 3.14;
+    let mut numero_mutable = 20;
+
+
+    println!("{}", numero);
+    println!("{}", frase);
+    println!("{}", f);
+    println!("{}", numero_mutable);
+    numero_mutable = 421;
+    println!("{}", numero_mutable);
+}
