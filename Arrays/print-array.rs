70e2c76b0ef1f87e90819785ec3816d331cc6c95
pablozuta <pablo.zuta.h@gmail.com>
2023-03-31 13:30:33 -0400

    se agrega carpeta de proyectos 'arrays' y modulo 'tuples'

---
+++ Arrays/print-array.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let my_array = [1, 20, 43, 433, 76, 43];
+    
+    for i in 0..my_array.len() {
+        println!("{}", my_array[i]);
+    }
+}
---
+++ tuples.rs
@@ -0,0 +1,6 @@
+
+fn main() {
+    let my_tuple = (42, "Hello", true);
+    println!("{:?}", my_tuple);
+    
+}
